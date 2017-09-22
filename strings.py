62597be435126a21e78c99578d23d64c4e57bc82
Happy Indra Wijaya <eng.hiwijaya@gmail.com>
2017-09-23 01:02:45 +0700

    add strings file

---
+++ strings.py
@@ -0,0 +1,12 @@
+a = 'I don\'t think so'
+print(a)
+
+# r --> raw string
+b = r'D:\hiwijaya\homework\nudePics'
+print(b)
+
+firstname = 'Happy '
+print(firstname + 'Indra Wijaya')
+
+# print 5 times
+print(firstname * 5)
