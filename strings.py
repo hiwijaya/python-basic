1e0cffe6c9e6b2b51c4a7ab4c914d2a0de0ab2d8
Happy Indra Wijaya <eng.hiwijaya@gmail.com>
2017-09-23 01:22:12 +0700

    vslicing up strings

--- strings.py
+++ strings.py
@@ -1,3 +1,4 @@
+# Use \ to ignore quotes inside quotes
 a = 'I don\'t think so'
 print(a)
 
@@ -10,3 +11,20 @@ print(firstname + 'Indra Wijaya')
 
 # print 5 times
 print(firstname * 5)
+
+# get element by index from string
+name = 'Happy Indra Wijaya'
+print(name[4])
+
+# get element by index start from the last
+print(name[-1])
+
+# get element by index with range
+print(name[2:8])
+
+# get element with range but start from the beginning
+print(name[:4])
+
+# get length of element string
+print(len(name))
+
