88af3e77e89f059263ce6059170dfcaaf5033db8
Happy Indra Wijaya <eng.hiwijaya@gmail.com>
2017-09-23 11:47:11 +0700

    collections tuple

--- collections.py
+++ collections.py
@@ -1,4 +1,5 @@
-# LIST
+# LIST ----------------------------------
+# List is a sequence data structure with index position. Similar with array in Java
 players = [10, 23, 24, 41]
 
 # get list item with index
@@ -29,5 +30,19 @@ players[:2] = []
 print(players)
 
 
+# TUPLE ----------------------------------
+# Like list but immutable. It mean the tuples cannot be changed.
+rooms = (4, 6, 1, 0.5, 7, 'eight', 'burger')
+single_room = (3,)  # to write a tuple containing single value, include a comma
 
+# get tuples item with index, similar with the list
+print(rooms[5])
 
+# tuple is immutable, but you are able to take values of existing tuple to create new tuples
+extra_rooms = (10, 13)
+alot_rooms = rooms + extra_rooms
+print(alot_rooms)
+
+# remove individual tuple element is not possible, but you can remove an entire tuple
+del alot_rooms
+print(alot_rooms)   # got NameError
