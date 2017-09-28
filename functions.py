b8f6387415f1677e2d26510ecd5bde959b89bcc2
Happy Indra Wijaya <eng.hiwijaya@gmail.com>
2017-09-28 22:10:27 +0700

    default parameter

--- functions.py
+++ functions.py
@@ -1,38 +1,45 @@
-# # function
-# def hallo():
-#     print('Hallo there..')
-# hallo()
-#
-#
-# # function with parameter
-# def hallo_you(name):
-#     print('Hallo ' + name)
-# hallo_you('Happy')
-#
-#
-# # function with single return value
-# def allow_dating_girl(my_age):
-#     girl_age = my_age/2 + 7
-#     return girl_age
-# allowed_girl_age = allow_dating_girl(25)
-# print(allowed_girl_age)
-#
-#
-# # function with many return value
-# def best_age(my_age):
-#     mid_age = my_age // 2
-#     next_age = my_age + 1
-#     return mid_age, next_age
-# (mid_age, next_age) = best_age(25)
-# print(mid_age, next_age)
+# function
+def hallo():
+    print('Hallo there..')
+hallo()
 
 
-# function with default parameter
-def get_sex(sex='unknown'):
-    if sex is 'm':
-        print('Male')
-    elif sex is 'f':
-        print('Female')
-    else:
-        print(sex)
-get_sex('m')
+# function with parameter
+def hallo_you(name):
+    print('Hallo ' + name)
+hallo_you('Happy')
+
+
+# function with single return value
+def allow_dating_girl(my_age):
+    girl_age = my_age/2 + 7
+    return girl_age
+allowed_girl_age = allow_dating_girl(25)
+print(allowed_girl_age)
+
+
+# function with many return value
+def best_age(my_age):
+    mid_age = my_age // 2
+    next_age = my_age + 1
+    return mid_age, next_age
+(mid_age, next_age) = best_age(25)
+print(mid_age, next_age)
+
+
+# function with keyword parameter/arguments
+def dumb_sentence(name='Happy', action='eat', item='burger'):
+    print(name, action, item)
+dumb_sentence()
+dumb_sentence('Nemo', 'split', 'gently')
+dumb_sentence(action='cook')
+dumb_sentence(item='sandwich', action='cook')
+
+
+# flexible number if arguments
+def add_numbers(*args):
+    total = 0
+    for i in args:
+        total += i
+    print('Total: ', total)
+add_numbers(1, 2, 3, 4, 5, 6)
