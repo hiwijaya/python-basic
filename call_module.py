4ed9a502c562a2beca7d2817b422869fe21599d2
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2020-04-19 14:40:34 +0700

    training module

---
+++ call_module.py
@@ -0,0 +1,10 @@
+import my_module
+from my_module import math
+
+# call module with this
+f = my_module.fibonacci(100)
+print(f)
+
+# or this
+f = math.fibonacci(100)
+print(f)
---
+++ my_module/math.py
@@ -0,0 +1,9 @@
+
+def fibonacci(n):
+    result = []
+    a, b = 0, 1
+    while b < n:
+        result.append(b)
+        a, b = b, a + b
+
+    return result
